394b80284bd3d3bf69467b22abe812aa015056dc
aryzle <seghaa@rpi.edu>
2014-11-04 21:58:06 -0500

    fixed date problem

--- app/models/foodpost.rb
+++ app/models/foodpost.rb
@@ -1,7 +1,7 @@
 class Foodpost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('date DESC, start_time DESC') }
-  default_scope -> { where('date > ?', Time.zone.now.beginning_of_day) }
+  default_scope -> { where('date >= ?', Time.now.beginning_of_day) }
   validates :content,    presence: true, length: { maximum: 500 }
   validates :user_id,    presence: true
   validates :date,       presence: true
